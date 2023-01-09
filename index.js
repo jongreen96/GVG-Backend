62a10458c2c18b4506147f66ac837c50e5dde2bb
Jon Green <jongreen1996@gmail.com>
2023-01-09 17:58:11 +0000

    Seperated route logic for scaling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const express = require('express'),
-	app = express(),
-	port = process.env.PORT;
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-	res.send('E-commerce REST API ');
-});
+// Directs all requests to main routes logic
+const routes = require('./routes');
+app.use('/', routes);
 
 app.listen(port, () => {
-	console.log(`Server listening on port: ${port}`);
+	console.log(`Server listening on port: http://localhost:${port}`);
 });
