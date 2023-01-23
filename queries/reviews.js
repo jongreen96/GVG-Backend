2558ad7b0ab7234a0183a0533ec347d635831a60
Jon Green <jongreen1996@gmail.com>
2023-01-23 22:12:53 +0000

    Improved reviews query

--- queries/reviews.js
+++ queries/reviews.js
@@ -13,7 +13,7 @@ module.exports = {
 			'SELECT product_id, user_id, score, description, images, created FROM reviews WHERE product_id = $1',
 			[productId]
 		);
-		if (!review) throw new Error('Review not found');
+		if (!review.rows[0]) throw new Error('Reviews not found');
 		return review.rows;
 	},
 	createReview: async (review) => {
@@ -25,18 +25,18 @@ module.exports = {
 	},
 	updateReview: async (id, newReview) => {
 		const review = await db.query('SELECT created FROM reviews WHERE id = $1', [id]);
-		if (!review) throw new Error('Review not found');
+		if (!review.rows[0]) throw new Error('Review not found');
 		if (review.created + 86400000 * 7 < Date.now())
 			throw new Error('Review cannot be updated after 7 days');
 		const result = await db.query(
-			'UPDATE reviews SET modified = NOW(), product_id = $1, user_id = $2, score = $3, description = $4 WHERE id = $5 RETURNING *',
+			'UPDATE reviews SET modified = NOW(), product_id = $1, user_id = $2, score = $3, description = $4 WHERE id = $5 RETURNING product_id, user_id, score, description, images',
 			[newReview.product_id, newReview.user_id, newReview.score, newReview.description, id]
 		);
 		return result.rows[0];
 	},
 	deleteReview: async (id) => {
 		const review = await db.query('SELECT created FROM reviews WHERE id = $1', [id]);
-		if (!review) throw new Error('Review not found');
+		if (!review.rows[0]) throw new Error('Review not found');
 		if (review.created + 86400000 * 7 < Date.now())
 			throw new Error('Review cannot be deleted after 7 days');
 		const result = await db.query('DELETE FROM reviews WHERE id = $1', [id]);
