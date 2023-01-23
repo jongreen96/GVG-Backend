a169813a8cbd7bdedca94574261e52893298d3b9
Jon Green <jongreen1996@gmail.com>
2023-01-23 15:29:56 +0000

    Improved auth query

--- queries/auth.js
+++ queries/auth.js
@@ -6,7 +6,7 @@ module.exports = {
 		if (user.password) user.password = await bcrypt.hash(user.password, 10);
 		const newUser = await db.query(
 			`INSERT INTO users (email, password, username, first_name, last_name, address) 
-			 VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+			 VALUES ($1, $2, $3, $4, $5, $6) RETURNING email, username, first_name, last_name, address`,
 			[
 				user.email,
 				user.password,
@@ -16,12 +16,6 @@ module.exports = {
 				user.address,
 			]
 		);
-		return {
-			email: newUser.rows[0].email,
-			username: newUser.rows[0].username,
-			firstName: newUser.rows[0].first_name,
-			lastName: newUser.rows[0].last_name,
-			address: newUser.rows[0].address,
-		};
+		return newUser.rows[0];
 	},
 };
