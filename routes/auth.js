dffbdf5aa63d8ceb8ba5771212bc5c3d7e8a349c
Jon Green <jongreen1996@gmail.com>
2023-03-20 12:48:36 +0000

    Updated auth to send user object

--- routes/auth.js
+++ routes/auth.js
@@ -1,42 +1,56 @@
-const authQuery = require('../queries/auth');
-const passport = require('passport');
-const { format } = require('../loaders/middleware');
+const authQuery = require('../queries/auth')
+const passport = require('passport')
+const { format } = require('../loaders/middleware')
 
 module.exports = (app) => {
 	// ------------------- GET ------------------- //
 	app.get('/auth', (req, res) => {
 		if (req.user) {
-			res.status(200).send({ message: 'User logged in' });
-		} else {	
-			res.status(400).send({ message: 'User not logged in' });
+			res.status(200).send({
+				user: {
+					email: req.user.email,
+					username: req.user.username,
+					first_name: req.user.first_name,
+					last_name: req.user.last_name,
+					address: req.user.address,
+				},
+			})
+		} else {
+			res.status(400).send({ message: 'User not logged in' })
 		}
-	});
+	})
 
 	// ------------------- POST ------------------- //
 	// Register new user
 	app.post('/register', format, async (req, res) => {
 		try {
-			const result = await authQuery.register(req.body);
-			res.send(result);
+			const result = await authQuery.register(req.body)
+			res.send(result)
 		} catch (err) {
-			err.column
-				? res.status(400).send({ message: `Missing input: ${err.column}` })
-				: res.status(400).send({ message: 'User not registered' });
+			err.column ? res.status(400).send({ message: `Missing input: ${err.column}` }) : res.status(400).send({ message: 'User not registered' })
 		}
-	});
+	})
 
 	// Login user
 	app.post('/login', passport.authenticate('local'), async (req, res) => {
-		res.status(200).redirect('/');
-	});
+		res.status(200).send({
+			user: {
+				email: req.user.email,
+				username: req.user.username,
+				first_name: req.user.first_name,
+				last_name: req.user.last_name,
+				address: req.user.address,
+			},
+		})
+	})
 
 	// Logout user
 	app.post('/logout', (req, res) => {
 		req.logout((err) => {
 			if (err) {
-				res.status(400).send({ message: 'User not logged out' });
+				res.status(400).send({ message: 'User not logged out' })
 			}
-		});
-		res.status(200).redirect('/');
-	});
-};
+		})
+		res.status(200).redirect('/')
+	})
+}
