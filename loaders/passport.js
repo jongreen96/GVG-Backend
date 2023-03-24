9b7261c6edeac4a148aa2c4800b1d6ede29eb86b
Jon Green <jongreen1996@gmail.com>
2023-03-24 13:16:37 +0000

    Refactored Passport LocalStrategy

--- loaders/passport.js
+++ loaders/passport.js
@@ -1,19 +1,19 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const db = require('../db');
-const bcrypt = require('bcryptjs');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const db = require('../db')
+const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
 	// Serialize user
 	passport.serializeUser((user, done) => {
-		done(null, user.id);
-	});
+		done(null, user.id)
+	})
 
 	// Deserialize user
 	passport.deserializeUser(async (id, done) => {
-		const user = await db.query('SELECT * FROM users WHERE id = $1', [id]);
-		done(null, user.rows[0]);
-	});
+		const user = await db.query('SELECT * FROM users WHERE id = $1', [id])
+		done(null, user.rows[0])
+	})
 
 	// Local strategy
 	passport.use(
@@ -23,21 +23,17 @@ module.exports = (app) => {
 				passwordField: 'password',
 			},
 			async (email, password, done) => {
-				email = email.toLowerCase();
-				const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
-				if (!user.rows[0]) {
-					return done(null, false, { message: 'User not found' });
-				}
-				const validPassword = await bcrypt.compare(password, user.rows[0].password);
-				if (!validPassword) {
-					return done(null, false, { message: 'Incorrect password' });
-				}
-				return done(null, user.rows[0]);
+				email = email.toLowerCase()
+				const user = await db.query('SELECT * FROM users WHERE email = $1', [email])
+				if (!user.rows[0]) return done(null, false, { message: 'User not found' })
+				const validPassword = await bcrypt.compare(password, user.rows[0].password)
+				if (!validPassword) return done(null, false, { message: 'Incorrect password' })
+				return done(null, user.rows[0])
 			}
 		)
-	);
+	)
 
 	// Initialize passport
-	app.use(passport.initialize());
-	app.use(passport.session());
-};
+	app.use(passport.initialize())
+	app.use(passport.session())
+}
