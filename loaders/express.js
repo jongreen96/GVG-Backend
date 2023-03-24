e8c775c9333d1256bbe1185a7888af8ca4a9d32e
Jon Green <jongreen1996@gmail.com>
2023-03-24 18:06:20 +0000

    Updated Cors options

--- loaders/express.js
+++ loaders/express.js
@@ -5,7 +5,12 @@ const pgSession = require('connect-pg-simple')(session);
 const { SESSION_SECRET } = require('../config');
 
 module.exports = (app) => {
-	app.use(cors());
+	app.use(
+		cors({
+			origin: 'http://localhost:5173',
+			credentials: true,
+		})
+	);
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(
