42ca44c2a3f8bfba2bfb37dd59e7c2d0b2d6ed7d
Jon Green <jongreen1996@gmail.com>
2023-03-25 15:39:42 +0000

    Updated auth to match incoming requests

--- queries/auth.js
+++ queries/auth.js
@@ -7,14 +7,7 @@ module.exports = {
 		const newUser = await db.query(
 			`INSERT INTO users (email, password, username, first_name, last_name, address) 
 			 VALUES ($1, $2, $3, $4, $5, $6) RETURNING email, username, first_name, last_name, address`,
-			[
-				user.email,
-				user.password,
-				user.username,
-				user.first_name,
-				user.last_name,
-				user.address,
-			]
+			[user.email, user.password, user.username, user.firstName, user.lastName, user.address]
 		);
 		return newUser.rows[0];
 	},
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
-const authQuery = require('../queries/auth')
-const passport = require('passport')
-const { format } = require('../loaders/middleware')
+const authQuery = require('../queries/auth');
+const passport = require('passport');
+const { format } = require('../loaders/middleware');
+const { json } = require('express');
 
 module.exports = (app) => {
 	// ------------------- GET ------------------- //
@@ -14,26 +15,26 @@ module.exports = (app) => {
 					last_name: req.user.last_name,
 					address: req.user.address,
 				},
-			})
+			});
 		} else {
-			res.status(204).send({ message: 'User not logged in' })
+			res.status(204).send({ message: 'User not logged in' });
 		}
-	})
+	});
 
 	// ------------------- POST ------------------- //
 	// Register new user
 	app.post('/register', format, async (req, res) => {
 		try {
-			const result = await authQuery.register(req.body)
-			res.send(result)
+			const result = await authQuery.register(req.body);
+			res.status(200).send(result);
 		} catch (err) {
-			err.column ? res.status(400).send({ message: `Missing input: ${err.column}` }) : res.status(400).send({ message: 'User not registered' })
+			err.column ? res.status(400).send({ message: `Missing input: ${err.column}` }) : res.status(400).send({ message: 'User not registered' });
 		}
-	})
+	});
 
 	// Login user
 	app.post('/login', passport.authenticate('local'), async (req, res) => {
-		res.status(200).send({
+		res.status(200).json({
 			user: {
 				email: req.user.email,
 				username: req.user.username,
@@ -41,16 +42,16 @@ module.exports = (app) => {
 				last_name: req.user.last_name,
 				address: req.user.address,
 			},
-		})
-	})
+		});
+	});
 
 	// Logout user
 	app.post('/logout', (req, res) => {
 		req.logout((err) => {
 			if (err) {
-				res.status(400).send({ message: 'User not logged out' })
+				res.status(400).send({ message: 'User not logged out' });
 			}
-		})
-		res.status(200).redirect('/')
-	})
-}
+		});
+		res.status(200).redirect('/');
+	});
+};
