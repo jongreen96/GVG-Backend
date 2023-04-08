dea0637d28383556060f07bfa6ee7e2d9b4d5727
Jon Green <jongreen1996@gmail.com>
2023-04-08 21:47:57 +0100

    Added confirmation email to customer

--- queries/contact.js
+++ queries/contact.js
@@ -10,15 +10,23 @@ const transporter = nodemailer.createTransport({
 
 module.exports = {
 	sendContactForm: async (name, email, message) => {
-		const mailOptions = {
+		const mailToMe = {
 			from: process.env.EMAIL,
 			to: process.env.EMAIL,
 			subject: `Contact form from ${name} - ${email}`,
 			text: message,
 		};
 
+		const MailToCustomer = {
+			from: process.env.EMAIL,
+			to: email,
+			subject: 'Thank you for contacting Green Vinyl Graphics',
+			text: `Thank you for contacting us, ${name.split(' ').shift()}. We will get back to you as soon as possible.`,
+		}
+
 		try {
-			await transporter.sendMail(mailOptions);
+			await transporter.sendMail(mailToMe);
+			await transporter.sendMail(MailToCustomer);
 		} catch (error) {
 			console.log(error);
 		}
