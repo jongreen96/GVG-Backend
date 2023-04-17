cd90831f94a9f656243a9d01c972bc488a652904
Jon Green <jongreen1996@gmail.com>
2023-04-17 11:33:52 +0100

    Added stripe endpoint

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,13 +1,26 @@
 const checkoutQueries = require('../queries/checkout');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = (app) => {
 	// --------------------- POST --------------------- //
 	app.post('/carts/checkout', async (req, res) => {
 		try {
-			const result = await checkoutQueries.createOrder(req.user.id, req.body);
+			const result = await checkoutQueries.createOrder(req.user.id);
 			res.send(result);
 		} catch (err) {
 			res.status(400).send({ message: err.message });
 		}
+	});
+
+	app.post('/create-payment-intent', async (req, res) => {
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: req.body.total * 100,
+			currency: 'gbp',
+			automatic_payment_methods: {
+				enabled: true,
+			},
+		});
+
+		res.send({ clientSecret: paymentIntent.client_secret });
 	});
 };
