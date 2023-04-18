8dccd694db251534bc4d25aa233b567e965093fd
Jon Green <jongreen1996@gmail.com>
2023-04-18 20:18:00 +0100

    Test removing dotenv entirely

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+// if (process.env.NODE_ENV !== 'production') {
+// 	require('dotenv').config();
+// }
 
 const app = require('express')();
 const port = process.env.PORT;
