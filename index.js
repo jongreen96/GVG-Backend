149ee2667b0a2371d57ca17aa57886b0725e198c
Jon Green <jongreen1996@gmail.com>
2023-04-18 21:00:58 +0100

    Undone dotenv changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV !== 'production') {
-// 	require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
 const app = require('express')();
 const port = process.env.PORT;
