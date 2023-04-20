64c21ac4c157c69012d60cfd358768a0bf6a19d5
Jon Green <jongreen1996@gmail.com>
2023-04-20 20:13:50 +0100

    used express.raw for webhook

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,4 +1,5 @@
 const checkoutQueries = require('../queries/checkout');
+const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
@@ -25,32 +26,36 @@ module.exports = (app) => {
 		res.send({ clientSecret: paymentIntent.client_secret });
 	});
 
-	app.post('/webhook', async (req, res) => {
-		const sig = req.headers['stripe-signature'];
-		let event;
-		try {
-			event = stripe.webhooks.constructEvent(
-				JSON.stringify(req.body), // Convert req.body to String
-				sig,
-				endpointSecret
-			);
-		} catch (err) {
-			res.status(400).send(`Webhook Error: ${err.message}`);
-			return;
-		}
+	app.post(
+		'/webhook',
+		express.raw({ type: 'application/json' }),
+		(req, res) => {
+			const sig = req.headers['stripe-signature'];
+			let event;
+			try {
+				event = stripe.webhooks.constructEvent(
+					request.body,
+					sig,
+					endpointSecret
+				);
+			} catch (err) {
+				response.status(400).send(`Webhook Error: ${err.message}`);
+				return;
+			}
 
-		// Handle the event
-		switch (event.type) {
-			case 'payment_intent.succeeded':
-				const paymentIntent = event.data.object;
-				console.log('PaymentIntent was successful!');
-				console.log(paymentIntent);
-				break;
-			default:
-				console.log(`Unhandled event type ${event.type}`);
-		}
+			// Handle the event
+			switch (event.type) {
+				case 'payment_intent.succeeded':
+					const paymentIntent = event.data.object;
+					console.log('PaymentIntent was successful!');
+					console.log(paymentIntent);
+					break;
+				default:
+					console.log(`Unhandled event type ${event.type}`);
+			}
 
-		// Return a response to acknowledge receipt of the event
-		res.send();
-	});
+			// Return a response to acknowledge receipt of the event
+			res.send();
+		}
+	);
 };
