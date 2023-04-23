f89645da7facd26cdce1f0b52f6254918f6bcc7e
Jon Green <jongreen1996@gmail.com>
2023-04-23 17:53:33 +0100

    Added all product images

--- routes/images.js
+++ routes/images.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
 	app.get('/images/:image', (req, res) => {
 		const { image } = req.params;
 		res.sendFile(image, {
-			root: './public',
+			root: './public/images',
 		});
 	});
 };
