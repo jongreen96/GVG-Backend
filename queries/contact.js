81b6d13554295926561fd3d9eedda2ebc9c06467
Jon Green <jongreen1996@gmail.com>
2023-04-27 14:31:32 +0100

    Added order confirmation email

--- queries/contact.js
+++ queries/contact.js
@@ -21,8 +21,37 @@ module.exports = {
 			from: process.env.EMAIL,
 			to: email,
 			subject: 'Thank you for contacting Green Vinyl Graphics',
-			text: `Thank you for contacting us, ${name.split(' ')[0]}. We will get back to you as soon as possible.`,
+			text: `Thank you for contacting us, ${name.split(' ')[0]}. 
+			We will get back to you as soon as possible.`,
+		};
+
+		try {
+			await transporter.sendMail(mailToMe);
+			await transporter.sendMail(MailToCustomer);
+		} catch (error) {
+			console.log(error);
 		}
+	},
+
+	sendOrderConfirmation: async (name, email, total) => {
+		const mailToMe = {
+			from: process.env.EMAIL,
+			to: process.env.EMAIL,
+			subject: `Order from ${name} - £${total} - ${email}`,
+			text: `Name: ${name}\nEmail: ${email}\nTotal: £${total}`,
+		};
+
+		const MailToCustomer = {
+			from: process.env.EMAIL,
+			to: email,
+			subject: 'Thank you for your order from Green Vinyl Graphics',
+			text: `Dear ${name.split(' ')[0]},\n\n
+			We can confirm that we have received your order from Green Vinyl Graphics. Thank you for choosing our online store for your digital template needs.\n\n
+			Your order is being processed, once completed visit your account page to download your files.\n
+			If you need any assistance with the download process or have any questions about your purchase, please contact us by replying to this email or messaging us via our contact form at www.greenvinylgraphics.com/about.\n\n
+			Thank you for choosing Green Vinyl Graphics, and we hope to serve you again in the future.\n
+			Sincerely,\nJon Green.`,
+		};
 
 		try {
 			await transporter.sendMail(mailToMe);
--- routes/contact.js
+++ routes/contact.js
@@ -6,10 +6,21 @@ module.exports = (app) => {
 	app.post('/contact', async (req, res) => {
 		try {
 			const { name, email, message } = req.body;
-			const result = await contactQueries.sendContactForm(
+			const result = await contactQueries.sendContactForm(name, email, message);
+			res.send(result);
+		} catch (err) {
+			res.status(400).send({ message: err.message });
+		}
+	});
+
+	// Send order confirmation
+	app.post('/order-confirmation', async (req, res) => {
+		try {
+			const { name, email, total } = req.body;
+			const result = await contactQueries.sendOrderConfirmation(
 				name,
 				email,
-				message
+				total
 			);
 			res.send(result);
 		} catch (err) {
